eced37eae2ec51bea7aa28b5f5cc30106fedc104
priyankaghiya <priyankaaghiya@gmail.com>
2024-07-13 18:09:02 +0530

    Updated the index file to solve the sonarlint Errors and indendation issues fix

--- php/index.php
+++ php/index.php
@@ -1,58 +1,66 @@
 <!DOCTYPE html>
-<head>
-  <title>Career Guidance</title>
-  <link rel="stylesheet" type="text/css" href="../css/Main.css">
- <link rel="stylesheet" type="text/css" href="../css/header.css">
-     <meta name="viewpoint" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-  <?php include("header.php"); ?>
-    <center>
-<table class="images">
-  <tr>
-  <td>
-  <div class="row">
-    <div class="column">      
-        <a href="After10th.php"><img src="../images/After10.jpg" class="images"></a>
-        <div class="middle">
-         <div class="text"><a href="../images/After10th.php" >After 10th class</a></div>
-        </div> 
-    </div>
-  </div></td>
-    <td>
-  <div class="row">
-    <div class="column">
-          <a href="https://careerbot.herokuapp.com/"><img src="../images/happy.png"class="images"></a>
-    <div class="middle">
-         <div class="text">AI ChatBot</div>
-    </div>
-  </div>
-</div></td>
-<td>
-     <div class="row">
-    <div class="column">
-        <a href="After12th.php"><img src="../images/after12th.jpg"class="images"></a>
-    <div class="middle">
-          <div class="text">After 12th class</div>
-    </div> 
-  </div>
-</div></td>
-      </tr>      
- <tr><td>
-<footer>
-  <a href="#" class="fa fa-facebook"></a>
-  <a href="#" class="fa fa-twitter"></a>
-  <a href="#" class="fa fa-google"></a>
-  <a href="#" class="fa fa-linkedin"></a>
-</footer>
-</td></tr>
-
-</table>
-       
-</center>
-</body>
+  <head>
+    <title>Career Guidance</title>
+    <link rel="stylesheet" type="text/css" href="../css/Main.css">
+  <link rel="stylesheet" type="text/css" href="../css/header.css">
+      <meta name="viewpoint" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <?php include("header.php"); ?>
+      <center>
+        <table class="images">
+          <tr><th></th></tr>
+          <tr>
+            <td>
+              <!-- For the Display of After 10th Class. -->
+              <div class="row">
+                <div class="column">
+                    <a href="After10th.php"><img src="../images/After10.jpg" alt="After 10th" class="images"></a>
+                    <div class="middle">
+                      <div class="text"><a href="../images/After10th.php" >After 10th class</a></div>
+                    </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <!-- For the Display of Robot Image -->
+              <div class="row">
+                <div class="column">
+                    <a href="https://careerbot.herokuapp.com/"><img src="../images/happy.png" alt="robot" class="images"></a>
+                    <div class="middle">
+                        <div class="text">AI ChatBot</div>
+                    </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <!-- For the Display of After 12th Class. -->
+              <div class="row">
+                <div class="column">
+                  <a href="After12th.php"><img src="../images/after12th.jpg" alt="After 12th" class="images"></a>
+                  <div class="middle">
+                        <div class="text">After 12th class</div>
+                  </div>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <footer>
+              <!-- Facebook -->
+              <a href="#" class="fa fa-facebook"><span></span></a>
+              <!-- Twitter -->
+              <a href="#" class="fa fa-twitter"><span></span></a>
+              <!-- Google -->
+              <a href="#" class="fa fa-google"><span></span></a>
+              <!-- LinkedIn --> 
+              <a href="#" class="fa fa-linkedin"><span></span></a>
+            </footer>
+            </td>
+          </tr>
+        </table>
+      </center>
+  </body>
 </html>
-
-
-
