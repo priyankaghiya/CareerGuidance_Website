84dd9d8242fc2d6b6b6daa4cce23dfe05976656f
priyankaghiya <priyankaaghiya@gmail.com>
2024-07-13 19:01:49 +0530

    Fixed the indendation issue for the feedback and aboutus files

--- php/aboutus.php
+++ php/aboutus.php
@@ -1,42 +1,50 @@
-<html>
-<head>
-<title>Career Guidance</title>
-<meta name="viewpoint" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="../css/aboutus.css">
-<link rel="stylesheet" type="text/css" href="../css/header.css">
-</head>
-<body>
-  <?php include("header.php"); ?>
-<div class="head">
-<h1> TEAM MEMEBERS </h1></div>
-<table>
-<tr>
-<th>Priyanka Ghiya</th>
-<th>Meet Bhavsar</th>
-<th>Aarti Chawda</th>
-<th>Vatsh Gandhi</th>
-</tr>
-<tr>
-<td><img src="../images/priyanka.jpeg" class="imgs"> </td>
-<td><img src="../images/meet.jpeg" class="imgs"> </td>
-<td><img src="../images/aarti.jpeg" class="imgs"> </td>
-<td><img src="../images/vatsh.jpeg" class="imgs im"> </td>
-</tr>
-</table>
-<br><Br><br>
-<center>
-<div class="head">
-<H1> CONTACT US </H1></div>
-<table>
-<tr>
-<th>BY PHONE </th>
-<th>LIVE CHAT</th>
-</tr>
-<tr>
-<td><img src="../images/cu1.jpg" class="vv"></td>
-<td><img src="../images/cu2.jpg" class="v"></td>
-</tr>
-</table>
-</center>
-</body>
+<!DOCTYPE html>
+  <head>
+    <title>Career Guidance</title>
+    <meta name="viewpoint" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/aboutus.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
+  </head>
+  <body>
+     <!-- To Display Header on the AboutUs Page -->
+    <?php include("header.php");
+    ?>
+    <!-- Heading of the page about us. -->
+    <div class="head">
+        <h1> TEAM MEMEBERS </h1>
+    </div>
+    <!-- Members name table -->
+    <table>
+        <!-- Row to display the members name. -->
+        <tr>
+        <th>Priyanka Ghiya</th>
+        <th>Meet Bhavsar</th>
+        <th>Aarti Chawda</th>
+        <th>Vatsh Gandhi</th>
+        </tr>
+        <!-- Row to display the images of the members -->
+        <tr>
+        <td><img src="../images/priyanka.jpeg" alt="priyankaGhiya" class="imgs"> </td>
+        <td><img src="../images/meet.jpeg" alt="meetBhavsar" class="imgs"> </td>
+        <td><img src="../images/aarti.jpeg" alt="aartiChawda" class="imgs"> </td>
+        <td><img src="../images/vatsh.jpeg" alt="vatshGandhi" class="imgs im"> </td>
+        </tr>
+    </table>
+    <br><Br><br>
+    <center>
+    <div class="head">
+        <h1> CONTACT US </h1>
+    </div>
+        <table>
+            <tr>
+                <th>BY PHONE </th>
+                <th>LIVE CHAT</th>
+            </tr>
+            <tr>
+                <td><img src="../images/cu1.jpg" alt="call" class="vv"></td>
+                <td><img src="../images/cu2.jpg" alt="chat" class="v"></td>
+            </tr>
+        </table>
+    </center>
+  </body>
 </html>
